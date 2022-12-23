83f4f7137d4b8c51618fd037d057e6a85ecf0938
ChrisElliotUK <christopher.elliot6@gmail.com>
2022-12-23 01:00:40 +0000

    add page animations

---
+++ lib/core/presentation/widgets/page_animations/shared_y_axis_transition_route_builder.dart
@@ -0,0 +1,43 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+
+class SharedYAxisTransitionRouteBuilder<T> extends PageRouteBuilder<T> {
+  /// The page to be transitioned to.
+  final Widget page;
+
+  /// The duration of the transition.
+  ///
+  /// Defaults to 300ms
+  final Duration? pageTransitionDuration;
+
+  /// The duration of the reverse transition
+  ///
+  /// Defaults to 300ms
+  final Duration? pageReverseTransitionDuration;
+
+  /// The color to fill the background with during the transition.
+  ///
+  /// Defaults to the [ThemeData.cardColor]
+  final Color? fillColor;
+
+  SharedYAxisTransitionRouteBuilder(
+      {required this.page,
+      required super.settings,
+      this.fillColor,
+      this.pageReverseTransitionDuration,
+      this.pageTransitionDuration})
+      : super(
+          transitionDuration: pageTransitionDuration ?? const Duration(milliseconds: 300),
+          reverseTransitionDuration: pageReverseTransitionDuration ?? const Duration(milliseconds: 300),
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return SharedAxisTransition(
+              fillColor: fillColor ?? Theme.of(context).cardColor,
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              transitionType: SharedAxisTransitionType.vertical,
+              child: child,
+            );
+          },
+        );
+}
